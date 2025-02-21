4fb976a7a16ec6d0728997558c9b28d2cc14a6b6
Madris1402 <ricmad142@gmail.com>
2025-02-21 12:52:50 -0600

    Busqueda de Logs, Lectura de Binarios

--- Clases/Sesion 05/Logs.sql
+++ Clases/Sesion 05/Logs.sql
@@ -18,7 +18,7 @@ show variables like '%log_file%';
 show variables like '%general_log%';
 
 set global general_log = 1;
-set global general_log_file = 'Log_2025_12_09.log';
+set global general_log_file = '.log';
 
 set global general_log = 0;
 
@@ -33,3 +33,37 @@ credito.consumo join credito.cuenta
 join credito.empleado 
 join credito.estado
 join credito.tarjeta;
+
+
+
+-- 21-02-2025
+show variables like '%log_bin%'; -- Mostrar 
+
+show binary logs;
+
+
+-- Transacciones
+use credito; 
+select * from depto;
+
+select version();
+start transaction;
+insert into depto values (18, 'ICO', 'ICO');
+insert into depto values (19, 'CIMA', 'CIMA');
+commit;
+
+start transaction;
+update depto set loc = 'MEXICO' where deptono in (18, 19);
+commit;
+
+-- Uso del Rollback para evitar los cambios
+start transaction;
+update depto set loc = 'MEXICO' where deptono> 0;
+select * from depto;
+rollback;
+
+select * from depto;
+
+show variables like '%commit%';
+
+show binary logs;
