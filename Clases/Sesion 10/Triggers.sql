a69082f69ca8ee9a3582356b9d918f6db8134c6e
Madris1402 <ricmad142@gmail.com>
2025-05-07 16:48:00 -0600

    Triggers para tabla persona

--- Clases/Sesion 10/Triggers.sql
+++ Clases/Sesion 10/Triggers.sql
@@ -198,3 +198,113 @@ delimiter ;
 delete from empleado where empno >= 13001;
 
 select * from bitacora;
+
+-- -- 07 - 05 - 2025
+-- Edad empleado
+create table persona as
+select x.empno, empleado, (year(now())- year(fnac)) edad from
+(select empno, curp,
+if((substr(curp, 5, 2) != ''),
+	concat(if(substr(curp, 5, 2) < 20, '20', '19'), substr(curp, 5, 2), 
+	'-', substr(curp, 7, 2), '-', substr(curp, 9, 2)), '') fnac
+from info_empleado) x
+JOIN (select empno, concat_ws(' ', enombre, epaterno, ematerno ) empleado from empleado) e on(e.empno = x.empno);
+
+create table edad_promedio as
+select truncate(avg(edad), 2) as promedio_edad from persona;
+
+select * from edad_promedio;
+
+-- Trigger After Insert
+delimiter $$
+drop trigger if exists ai_persona $$
+create trigger ai_persona
+after insert on persona
+for each row
+begin
+	update edad_promedio set promedio_edad = (select truncate(avg(edad), 2) as promedio_edad from persona);
+end $$
+delimiter ;
+
+-- Trigger Before Insert
+delimiter $$
+drop trigger if exists bi_persona $$
+create trigger bi_persona
+before insert on persona
+for each row
+begin
+	if(new.edad < 18) then
+		signal sqlstate '45000'
+		set message_text = 'La persona debe ser mayor de edad';
+    end if;
+end $$
+delimiter ;
+
+-- Trigger After Update
+delimiter $$
+drop trigger if exists au_persona $$
+create trigger au_persona
+after update on persona
+for each row
+begin
+	update edad_promedio set promedio_edad = (select truncate(avg(edad), 2) as promedio_edad from persona);
+end $$
+delimiter ;
+
+-- Trigger Before Update
+delimiter $$
+drop trigger if exists bu_persona $$
+create trigger bu_persona
+before update on persona
+for each row
+begin
+	if(new.edad < 18) then
+		signal sqlstate '45000'
+		set message_text = 'La persona debe ser mayor de edad';
+    end if;
+end $$
+delimiter ;
+
+-- Trigger After Delete
+delimiter $$
+drop trigger if exists ad_persona $$
+create trigger ad_persona
+after delete on persona
+for each row
+begin
+	update edad_promedio set promedio_edad = (select truncate(avg(edad), 2) as promedio_edad from persona);
+end $$
+delimiter ;
+
+-- Trigger Before Delete
+delimiter $$
+drop trigger if exists bd_persona $$
+create trigger bd_persona
+before delete on persona
+for each row
+begin
+	insert into bitacora values(null, sysdate(), user(), 'persona', 
+    json_object('action', 'ELIMINACION', 
+    'empno', old.empno, 
+    'empleado', old.empleado,
+    'edad', old.edad)
+    );
+end $$
+delimiter ;
+
+
+show triggers;
+
+set sql_safe_updates = 0;
+insert into persona values ('13001', 'MARTHA RIOS LAGUNAS', 70);
+insert into persona values ('13002', 'OSCAR RIOS LAGUNAS', 16);
+insert into persona values ('13002', 'OSCAR RIOS LAGUNAS', 60);
+
+select * from persona;
+select * from edad_promedio;
+
+update persona set edad = edad + 7 where empno >= 13001;
+update persona set edad = 17 where empno >= 13001;
+
+delete from persona where empno >= 13001;
+select * from bitacora;
